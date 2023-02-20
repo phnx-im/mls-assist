ec3b78825b22cb76326120ea2d63d6dbee763096
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2023-02-20 15:27:39 +0100

    refactor processing slightly

--- src/group/mod.rs
+++ src/group/mod.rs
@@ -5,14 +5,15 @@ use openmls::{
         CreationFromExternalError, ProposalStore, PublicGroup,
     },
     prelude::{
-        LibraryError, OpenMlsSignaturePublicKey, ProcessedMessage, ProcessedMessageContent, Sender,
+        ConfirmationTag, LibraryError, OpenMlsSignaturePublicKey, ProcessedMessage,
+        ProcessedMessageContent, Sender, StagedCommit,
     },
     treesync::{LeafNode, Node},
 };
 use openmls_rust_crypto::{OpenMlsCryptoProvider, OpenMlsRustCrypto};
 use serde::{Deserialize, Serialize};
 
-use crate::messages::AssistedMessage;
+use crate::messages::{AssistedCommit, AssistedGroupInfo, AssistedMessage};
 
 use self::errors::ProcessAssistedMessageError;
 
@@ -64,64 +65,31 @@ impl Group {
                     .process_message(self.backend(), public_message)?;
                 Ok(ProcessedAssistedMessage::NonCommit(processed_message))
             }
-            AssistedMessage::Commit(commit) => {
-                // First process the message, the check if the group info checks out.
+            AssistedMessage::Commit(AssistedCommit {
+                commit,
+                assisted_group_info,
+            }) => {
+                // First process the message, then verify that the group info
+                // checks out.
                 let processed_message = self
                     .public_group
-                    .process_message(self.backend(), commit.commit.clone())?;
+                    .process_message(self.backend(), commit.clone())?;
                 let sender = processed_message.sender().clone();
+                let confirmation_tag = commit
+                    .confirmation_tag()
+                    .ok_or(LibraryError::custom(
+                        "No confirmation tag in commit after validation.",
+                    ))?
+                    .clone();
                 if let ProcessedMessageContent::StagedCommitMessage(staged_commit) =
                     processed_message.content()
                 {
-                    let group_info: GroupInfo = {
-                        let sender_index = match sender {
-                            Sender::Member(leaf_index) => leaf_index,
-                            Sender::NewMemberCommit => {
-                                self.public_group.free_leaf_index_after_remove(
-                                    staged_commit.inline_proposals().map(|p| p.proposal()),
-                                )?
-                            }
-                            Sender::External(_) | Sender::NewMemberProposal => {
-                                return Err(ProcessAssistedMessageError::LibraryError(
-                                    LibraryError::custom("Invalid sender after validation."),
-                                ))
-                            }
-                        };
-                        let confirmation_tag = commit
-                            .commit
-                            .confirmation_tag()
-                            .ok_or(LibraryError::custom(
-                                "No confirmation tag in commit after validation.",
-                            ))?
-                            .clone();
-                        let verifiable_group_info =
-                            commit.assisted_group_info.into_verifiable_group_info(
-                                sender_index,
-                                staged_commit.staged_context().clone(),
-                                confirmation_tag,
-                            );
-
-                        let sender_pk = self
-                            .public_group
-                            .members()
-                            .find_map(|m| {
-                                if m.index == sender_index {
-                                    Some(m.signature_key)
-                                } else {
-                                    None
-                                }
-                            })
-                            .map(|pk_bytes| {
-                                OpenMlsSignaturePublicKey::from_signature_key(
-                                    pk_bytes.into(),
-                                    verifiable_group_info.ciphersuite().into(),
-                                )
-                            })
-                            .ok_or(ProcessAssistedMessageError::UnknownSender)?;
-                        verifiable_group_info
-                            .verify(self.backend().crypto(), &sender_pk)
-                            .map_err(|_| ProcessAssistedMessageError::InvalidGroupInfoSignature)?
-                    };
+                    let group_info: GroupInfo = self.validate_group_info(
+                        sender,
+                        staged_commit,
+                        confirmation_tag,
+                        assisted_group_info,
+                    )?;
                     // This is really only relevant for the "Full" group info case above.
                     if group_info.group_context() != staged_commit.staged_context() {
                         return Err(ProcessAssistedMessageError::InconsistentGroupContext);
@@ -150,13 +118,55 @@ impl Group {
         self.public_group.finalize_processing(processed_message)
     }
 
-    pub fn public_tree() {}
-
     pub fn group_info(&self) -> &GroupInfo {
         &self.group_info
     }
 
-    pub fn members() {}
+    fn validate_group_info(
+        &self,
+        sender: Sender,
+        staged_commit: &StagedCommit,
+        confirmation_tag: ConfirmationTag,
+        assisted_group_info: AssistedGroupInfo,
+    ) -> Result<GroupInfo, ProcessAssistedMessageError> {
+        let sender_index = match sender {
+            Sender::Member(leaf_index) => leaf_index,
+            Sender::NewMemberCommit => self.public_group.free_leaf_index_after_remove(
+                staged_commit.inline_proposals().map(|p| p.proposal()),
+            )?,
+            Sender::External(_) | Sender::NewMemberProposal => {
+                return Err(ProcessAssistedMessageError::LibraryError(
+                    LibraryError::custom("Invalid sender after validation."),
+                ))
+            }
+        };
+        let verifiable_group_info = assisted_group_info.into_verifiable_group_info(
+            sender_index,
+            staged_commit.staged_context().clone(),
+            confirmation_tag,
+        );
+
+        let sender_pk = self
+            .public_group
+            .members()
+            .find_map(|m| {
+                if m.index == sender_index {
+                    Some(m.signature_key)
+                } else {
+                    None
+                }
+            })
+            .map(|pk_bytes| {
+                OpenMlsSignaturePublicKey::from_signature_key(
+                    pk_bytes.into(),
+                    verifiable_group_info.ciphersuite().into(),
+                )
+            })
+            .ok_or(ProcessAssistedMessageError::UnknownSender)?;
+        verifiable_group_info
+            .verify(self.backend().crypto(), &sender_pk)
+            .map_err(|_| ProcessAssistedMessageError::InvalidGroupInfoSignature)
+    }
 }
 
 pub enum ProcessedAssistedMessage {
