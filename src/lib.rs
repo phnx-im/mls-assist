4c7445da6eaba7d56b9245d020d792fb05b7583e
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2023-03-01 15:09:41 +0100

    add export

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub use openmls::messages::group_info::VerifiableGroupInfo;
 pub use openmls::prelude::{
     GroupEpoch, GroupId, KeyPackage, LeafNode, LeafNodeIndex, OpenMlsCrypto, OpenMlsCryptoProvider,
-    Sender, SignaturePublicKey, SignatureScheme,
+    ProcessedMessageContent, Sender, SignaturePublicKey, SignatureScheme,
 };
 pub use openmls_rust_crypto::OpenMlsRustCrypto;
 
