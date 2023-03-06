279b72968b6ce89774d175a55c60e2cfcbc85f23
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2023-03-06 09:13:28 +0100

    re-export more things from OpenMLS

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 pub use openmls::messages::group_info::VerifiableGroupInfo;
 pub use openmls::prelude::{
-    GroupEpoch, GroupId, KeyPackage, KeyPackageRef, LeafNode, LeafNodeIndex, OpenMlsCrypto,
-    OpenMlsCryptoProvider, ProcessedMessageContent, Sender, SignaturePublicKey, SignatureScheme,
+    Extensions, GroupEpoch, GroupId, KeyPackage, KeyPackageRef, LeafNode, LeafNodeIndex,
+    OpenMlsCrypto, OpenMlsCryptoProvider, ProcessedMessageContent, QueueConfigExtension, Sender,
+    SignaturePublicKey, SignatureScheme,
 };
 pub use openmls_rust_crypto::OpenMlsRustCrypto;
 
