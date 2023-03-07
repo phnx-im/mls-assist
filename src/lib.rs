deef8000ecf93aa3ebbacb3288bc7f705a4d328a
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2023-03-07 14:37:51 +0100

    refactor exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 pub use openmls::messages::group_info::VerifiableGroupInfo;
-pub use openmls::prelude::{
-    Extensions, GroupEpoch, GroupId, KeyPackage, KeyPackageRef, LeafNode, LeafNodeIndex, Member,
-    Node, OpenMlsCrypto, OpenMlsCryptoProvider, ProcessedMessageContent, QueueConfigExtension,
-    Sender, SignaturePublicKey, SignatureScheme,
+pub use openmls::{
+    prelude::{
+        Extensions, GroupEpoch, GroupId, KeyPackage, KeyPackageRef, LeafNodeIndex, Member,
+        OpenMlsCrypto, OpenMlsCryptoProvider, ProcessedMessageContent, QueueConfigExtension,
+        Sender, SignaturePublicKey, SignatureScheme,
+    },
+    treesync::{LeafNode, Node},
 };
 pub use openmls_rust_crypto::OpenMlsRustCrypto;
 
