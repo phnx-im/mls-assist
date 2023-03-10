e80b585d60ceb34c43be4524584303e81df2dea9
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2023-03-10 16:28:09 +0100

    export hash type

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,10 @@ pub use openmls::messages::group_info::VerifiableGroupInfo;
 pub use openmls::{
     framing::ProcessedMessage,
     prelude::{
-        group_info::GroupInfo, Extensions, GroupEpoch, GroupId, KeyPackage, KeyPackageRef,
-        LeafNodeIndex, Member, OpenMlsCrypto, OpenMlsCryptoProvider, ProcessedMessageContent,
-        Proposal, QueueConfigExtension, QueuedRemoveProposal, Sender, SignaturePublicKey,
-        SignatureScheme, StagedCommit,
+        group_info::GroupInfo, Extensions, GroupEpoch, GroupId, HashType, KeyPackage,
+        KeyPackageRef, LeafNodeIndex, Member, OpenMlsCrypto, OpenMlsCryptoProvider,
+        ProcessedMessageContent, Proposal, QueueConfigExtension, QueuedRemoveProposal, Sender,
+        SignaturePublicKey, SignatureScheme, StagedCommit,
     },
     treesync::{LeafNode, Node},
 };
