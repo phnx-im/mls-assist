b1cc416abf88311153b5c09451ff55d8deaaee57
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2023-05-25 15:53:46 +0200

    re-export OpenMLS fully

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,5 @@
-pub use openmls::messages::group_info::VerifiableGroupInfo;
-pub use openmls::{
-    prelude::{group_info::GroupInfo, *},
-    treesync::{LeafNode, Node},
-    *,
-};
-pub use openmls_rust_crypto::OpenMlsRustCrypto;
+pub use openmls;
+pub use openmls_rust_crypto;
 
 pub mod group;
 pub mod messages;
