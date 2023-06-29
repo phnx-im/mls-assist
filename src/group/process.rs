04497e57fcacdb921ceddae496b46c34c25c769d
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2023-06-29 11:50:46 +0200

    add hack to make group info validation work

--- src/group/process.rs
+++ src/group/process.rs
@@ -1,4 +1,4 @@
-use openmls::prelude::{OpenMlsCryptoProvider, ProtocolMessage, Verifiable};
+use openmls::prelude::{MlsCredentialType, OpenMlsCryptoProvider, ProtocolMessage, Verifiable};
 
 use super::{errors::LibraryError, *};
 
@@ -33,8 +33,30 @@ impl Group {
                 if let ProcessedMessageContent::StagedCommitMessage(staged_commit) =
                     processed_message.content()
                 {
+                    let assisted_sender = match sender {
+                        Sender::Member(leaf_index) => AssistedSender::Member(leaf_index),
+                        Sender::NewMemberCommit => {
+                            // For now, the only way we can get hold of the signature key (before merging the commit) is to assume that it's an InfraCredential.
+                            let signature_key =
+                                match processed_message.credential().mls_credential_type() {
+                                    MlsCredentialType::Infra(infra_credential) => {
+                                        infra_credential.verifying_key().clone()
+                                    }
+                                    MlsCredentialType::Basic(_) | MlsCredentialType::X509(_) => {
+                                        // TODO: For now, this only supports InfraCredentials.
+                                        return Err(ProcessAssistedMessageError::UnknownSender);
+                                    }
+                                };
+                            AssistedSender::External(signature_key)
+                        }
+                        Sender::External(_) | Sender::NewMemberProposal => {
+                            return Err(ProcessAssistedMessageError::LibraryError(
+                                LibraryError::LibraryError, // Invalid sender after validation.
+                            ));
+                        }
+                    };
                     let group_info: GroupInfo = self.validate_group_info(
-                        sender,
+                        assisted_sender,
                         staged_commit,
                         confirmation_tag,
                         assisted_group_info,
@@ -57,25 +79,52 @@ impl Group {
     }
 }
 
+enum AssistedSender {
+    Member(LeafNodeIndex),
+    External(SignaturePublicKey),
+}
+
 // Helper functions
 impl Group {
     fn validate_group_info(
         &self,
-        sender: Sender,
+        sender: AssistedSender,
         staged_commit: &StagedCommit,
         confirmation_tag: ConfirmationTag,
         assisted_group_info: AssistedGroupInfoIn,
     ) -> Result<GroupInfo, ProcessAssistedMessageError> {
-        let sender_index = match sender {
-            Sender::Member(leaf_index) => leaf_index,
-            Sender::NewMemberCommit => self
-                .public_group
-                .ext_commit_sender_index(staged_commit)
-                .map_err(LibraryError::OpenMlsLibraryError)?,
-            Sender::External(_) | Sender::NewMemberProposal => {
-                return Err(ProcessAssistedMessageError::LibraryError(
-                    LibraryError::LibraryError, // Invalid sender after validation.
-                ));
+        let signature_scheme = self.group_info().group_context().ciphersuite().into();
+        let (sender_index, sender_pk) = match sender {
+            AssistedSender::Member(index) => {
+                let sender_pk = self
+                    .public_group
+                    .members()
+                    .find_map(|m| {
+                        if m.index == index {
+                            Some(m.signature_key)
+                        } else {
+                            None
+                        }
+                    })
+                    .map(|pk_bytes| {
+                        OpenMlsSignaturePublicKey::from_signature_key(
+                            pk_bytes.into(),
+                            signature_scheme,
+                        )
+                    })
+                    .ok_or(ProcessAssistedMessageError::UnknownSender)?;
+                (index, sender_pk)
+            }
+            AssistedSender::External(signature_public_key) => {
+                let index = self
+                    .public_group
+                    .ext_commit_sender_index(staged_commit)
+                    .map_err(LibraryError::OpenMlsLibraryError)?;
+                let openmls_signature_key = OpenMlsSignaturePublicKey::from_signature_key(
+                    signature_public_key,
+                    signature_scheme,
+                );
+                (index, openmls_signature_key)
             }
         };
         let verifiable_group_info = assisted_group_info
@@ -86,23 +135,6 @@ impl Group {
             )
             .map_err(|_| ProcessAssistedMessageError::InvalidGroupInfoMessage)?;
 
-        let sender_pk = self
-            .public_group
-            .members()
-            .find_map(|m| {
-                if m.index == sender_index {
-                    Some(m.signature_key)
-                } else {
-                    None
-                }
-            })
-            .map(|pk_bytes| {
-                OpenMlsSignaturePublicKey::from_signature_key(
-                    pk_bytes.into(),
-                    verifiable_group_info.ciphersuite().into(),
-                )
-            })
-            .ok_or(ProcessAssistedMessageError::UnknownSender)?;
         verifiable_group_info
             .verify(self.backend().crypto(), &sender_pk)
             .map_err(|_| ProcessAssistedMessageError::InvalidGroupInfoSignature)
