0cce518e08506b48b4939e0dfd3e90f64820a276
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2024-06-12 11:22:44 +0200

    adapt to storage provider changes in OpenMLS main

--- src/group/errors.rs
+++ src/group/errors.rs
@@ -1,12 +1,18 @@
 use openmls::prelude::ProcessMessageError;
+use openmls_traits::{
+    storage::StorageProvider as StorageProviderTrait, storage::CURRENT_VERSION, OpenMlsProvider,
+};
 use thiserror::Error;
 
 #[cfg(doc)]
 use openmls::prelude::{group_info::GroupInfo, GroupContext, ProcessedMessage};
 
+pub type StorageError<Provider> =
+    <<Provider as OpenMlsProvider>::StorageProvider as StorageProviderTrait<CURRENT_VERSION>>::Error;
+
 /// Process message error
 #[derive(Error, Debug, PartialEq, Clone)]
-pub enum ProcessAssistedMessageError {
+pub enum ProcessAssistedMessageError<StorageError> {
     /// Invalid assisted message.
     #[error("Invalid assisted message.")]
     InvalidAssistedMessage,
@@ -21,7 +27,7 @@ pub enum ProcessAssistedMessageError {
     InvalidGroupInfoMessage,
     /// See [`ProcessMessageError`] for more details.
     #[error(transparent)]
-    ProcessMessageError(#[from] ProcessMessageError),
+    ProcessMessageError(#[from] ProcessMessageError<StorageError>),
     /// Unknown sender.
     #[error("Unknown sender.")]
     UnknownSender,
